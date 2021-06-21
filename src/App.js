3beffd10117683a5ef79770c1b7d4b22e3c2578a
Jason Zhang <jasonzhang.syd@gmail.com>
2021-06-21 16:25:27 +1000

    complete changePage function and add PropTypes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Pagination from "./components/Pagination";
 const App = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [posts, setPosts] = useState([]);
-  const [postsPerPage, setPostsPerPage] = useState(10);
+  const [postsPerPage] = useState(10);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
@@ -24,12 +24,19 @@ const App = () => {
   const indexOfLastPost = currentPage * postsPerPage;
   const indexOfFirstPost = indexOfLastPost - postsPerPage;
   const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+  const changePage = page => {
+    setCurrentPage(page);
+  };
 
   return (
     <div className="container mt-5">
       <h1 className="text-primary mb-3">React Pagination Demo</h1>
       <Posts loading={loading} currentPosts={currentPosts} />
-      <Pagination postsPerPage={postsPerPage} totalPosts={posts.length} />
+      <Pagination
+        postsPerPage={postsPerPage}
+        totalPosts={posts.length}
+        changePage={changePage}
+      />
     </div>
   );
 };
--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,6 +1,7 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const Pagination = ({ postsPerPage, totalPosts }) => {
+const Pagination = ({ postsPerPage, totalPosts, changePage }) => {
   const pageNumbers = [];
   for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
     pageNumbers.push(i);
@@ -8,7 +9,11 @@ const Pagination = ({ postsPerPage, totalPosts }) => {
   return (
     <nav className="pagination">
       {pageNumbers.map(number => (
-        <li key={number} className="page-item">
+        <li
+          key={number}
+          className="page-item"
+          onClick={() => changePage(number)}
+        >
           <a href="!#" className="page-link">
             {number}
           </a>
@@ -16,6 +21,12 @@ const Pagination = ({ postsPerPage, totalPosts }) => {
       ))}
     </nav>
   );
+};
+
+Pagination.prototype = {
+  postsPerPage: PropTypes.number.isRequired,
+  totalPosts: PropTypes.number.isRequired,
+  changePage: PropTypes.func.isRequired,
 };
 
 export default Pagination;
